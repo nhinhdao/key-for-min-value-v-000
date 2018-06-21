812d50bbcf331720956802fb3cde9564a4266bec
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-21 03:45:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,9 @@ def key_for_min_value(name_hash)
       if !name_hash.empty?
             temp = 1000
             name_hash.each do |key, value|
-                  if value < temp
-                        temp = value
-                  end
+                  temp = value if value < temp
             end
-            temp
-            name_hash.each do |key, value|
-                  if value == temp
-                        return key
-                  end
-            end
-            key
+            name_hash.key(temp)
       else
             nil
       end
